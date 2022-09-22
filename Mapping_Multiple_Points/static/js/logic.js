50d2ea218301f1feca16daac24760662a2911500
Rida Riaz <ridariaz@Ridas-MacBook-Pro.local>
2022-09-21 20:32:54 -0700

    saving changes to mapping_lines branch

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -11,12 +11,18 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
     accessToken: API_KEY
 });
 
-//  Add a marker to the map for Los Angeles, California.
-L.circle([34.0522, -118.2437], {
-    radius: 300,
-    color: "black",
-    fillColor: '#ffffa1'
- }).addTo(map);
-
+// Get data from cities.js
+let cityData = cities;
+  
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location, {
+        radius: city.population/100000
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+  .addTo(map);
+});
+   
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
